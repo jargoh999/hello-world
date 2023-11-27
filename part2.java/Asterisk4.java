4bbae472a6b84b817ac64f257ddf0623cb05d8b7
jargoh999 <fatoyeayomide123456@gmail.com>
2023-11-27 03:23:15 +0100

    java

---
+++ part2.java/Asterisk4.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+public class Asterisk4{
+public static void main(String [] args){
+Scanner input = new Scanner(System.in);
+System.out.println(" enter number of asterisk to display ");
+int asterisk = input.nextInt();
+ 
+
+int Acounter = 1;
+
+        Acounter = 1;
+while ( Acounter <= asterisk){
+     
+       int Bcounter = asterisk;
+while ( Bcounter >= Acounter ){
+       System.out.print("*");
+       Bcounter--;
+      
+				}
+       System.out.println("");
+        Acounter++;
+				} 
+
+//?
+     Acounter = 1;
+
+while ( Acounter <= asterisk) { 
+
+     int Bcounter = asterisk ; 
+          
+while ( Bcounter >= Acounter ) { 
+   System.out.print(" ");
+    Bcounter--;        }  
+ 
+           int Ccounter = 1;       	
+while ( Ccounter <= Acounter){
+         System.out.print("*");
+	 Ccounter++;	}		
+      			 
+                            
+       System.out.println();
+          Acounter++;
+                         }
+
+
+
+//?                         
+Acounter = 1;
+
+
+while ( Acounter <= asterisk) { 
+
+     int Bcounter = 1; 
+          
+while ( Bcounter <= Acounter ) { 
+   System.out.print(" ");
+    Bcounter++;        }  
+ 
+        int Ccounter = asterisk;       	
+while ( Ccounter >= Acounter){
+         System.out.print("*");
+         Ccounter--;	
+				}		
+      			 
+                            
+       System.out.println();
+          Acounter++;
+                         }
+                		
+//  ?
+   Acounter = 1;
+while ( Acounter <= asterisk){
+     
+       int Bcounter = 1;
+while ( Bcounter <= Acounter ){
+       System.out.print("*");
+       Bcounter++;
+      
+				}
+       System.out.println("");
+        Acounter++;
+				}
+				
+                            }
+   				}
+ 
+
+
+
